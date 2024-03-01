14eda1c23de65d3f2938b59d4a8b806282b59a1c
Erick Leandro <j.erickleandro@gmail.com>
2024-03-01 19:46:18 +0000

    chore: testes vercel

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,6 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || "mongodb://localhost:27017/servico-news",
+  mongoUrl:
+    "mongodb+srv://bussola:7OCq3A0H9TYenFHf@bussola.7fh6rek.mongodb.net/?retryWrites=true&w=majority&appName=bussola",
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "jd826==sg",
 };
